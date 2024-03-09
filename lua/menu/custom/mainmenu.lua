e7f04bd92c7407449c91ff31e3114c3682eef9b7
Xenthio <thegooster88@gmail.com>
2024-03-09 15:56:36 +1100

    Add a dummy function to stop gmod from complaining about any errors related to the problems menu

--- lua/menu/custom/mainmenu.lua
+++ lua/menu/custom/mainmenu.lua
@@ -406,6 +406,11 @@ function PANEL:RefreshContent()
 
 end
 
+function PANEL:SetProblemCount( problems, severity )
+	
+	-- The lua menu doesn't have a problems menu. This function is only here to shut any errors up.
+end
+
 function PANEL:ScreenshotScan( folder )
 
 	local bReturn = false
